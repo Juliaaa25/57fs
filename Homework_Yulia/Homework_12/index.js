7e8b54151c07a7693aa1efc9f59a7fa62333bc90
Yulia Kashina <yulia.kaot@mail.ru>
2025-07-03 11:33:15 +0200

    Homework_12 is done

--- Homework_Yulia/Homework_12/index.js
+++ Homework_Yulia/Homework_12/index.js
@@ -5,11 +5,21 @@ const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
 
-btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  
-})
+// при нажатии на первую кнопку меняем текст
+btn.addEventListener("click", () => {
+  clonedBtn.textContent = "Я изменю тебя";
 
-// здесь можете создать EventListener для второй кнопки
+  // поменяем стили кнопки
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+
+  // добавляем новый класс и кнопку в body
+  clonedBtn.classList.add("second-btn");
+  document.body.appendChild(clonedBtn);
+});
+
+clonedBtn.addEventListener("click", () => {
+  // меняем стили первой кнопки
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
+});
