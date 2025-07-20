5c87945c341ad0be67cbd061bbe94c0cf49bb552
Yulia Kashina <yulia.kaot@mail.ru>
2025-07-20 19:11:26 +0200

    Consultation_9 (wine) is done

---
+++ Homework_Yulia/Homework_17/FAKE-STORE-APP2/js/categories.js
@@ -0,0 +1,26 @@
+const categoriesList = document.getElementById("categories-list");
+
+async function fetchCategories() {
+  try {
+    const res = await fetch("https://api.escuelajs.co/api/v1/categories");
+    const categories = await res.json();
+
+    categories.forEach((category) => {
+      const li = document.createElement("li");
+      li.textContent = category.name;
+
+      const img = document.createElement("img");
+      img.src = category.image;
+      img.alt = category.name;
+      img.style.width = "100px";
+
+      li.appendChild(img);
+      categoriesList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Ошибка при загрузке категорий:", error);
+    categoriesList.innerHTML = "<li>Ошибка загрузки категорий 😢</li>";
+  }
+}
+
+fetchCategories();
